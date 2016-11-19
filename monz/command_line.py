4df02c0e42da555148764ef132834e80dc0f6bea
Paweł Adamczak <pawel.ad@gmail.com>
2016-11-19 17:38:26 +0000

    Added 'balance' subcommand

--- monz/command_line.py
+++ monz/command_line.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, division, print_function
 
+from decimal import Decimal
+
 import click
 import dateutil.parser
+from babel.numbers import format_currency
 from pymonzo import MonzoAPI
 
 
@@ -25,16 +28,41 @@ def accounts(ctx):
     for n, account in enumerate(monzo_accounts, start=1):
         click.secho(
             "Account #{}, {}".format(n, account['description']),
-            fg='green'
+            fg='green',
         )
 
-        click.echo("\t{0:<8} {1}".format('id', account['id']))
-        click.echo("\t{0:<8} {1:%b %-d, %Y %-I:%M %p}".format(
-            'created', dateutil.parser.parse(account['created']))
+        click.echo('{0:<12} {1}'.format('ID:', account['id']))
+        click.echo('{0:<12} {1:%b %-d, %Y %-I:%M %p}'.format(
+            'Created:', dateutil.parser.parse(account['created']))
         )
 
         if n != len(monzo_accounts):
             click.echo()  # Print new line between accounts
+
+
+@cli.command()
+@click.option('--account-id', '-a', type=str, help="Monzo account ID.")
+@click.pass_context
+def balance(ctx, account_id):
+    """Show Monzo account balance"""
+    try:
+        monzo_balance = ctx.obj['monzo_api'].balance(account_id=account_id)
+    except ValueError as e:
+        raise click.ClickException(e)
+
+    # API returns balance without the delimiter before the subunit,
+    # so 12345 is in fact 123.45
+    value = Decimal(
+        str(monzo_balance['balance'])[:-2] + '.' +
+        str(monzo_balance['balance'])[-2:]
+    )
+    local_value = format_currency(value, monzo_balance['currency'])
+    local_spent_today = format_currency(
+        monzo_balance['spend_today'], monzo_balance['currency']
+    )
+
+    click.secho('{0:<12} {1}'.format('Balance:', local_value), fg='green')
+    click.echo('{0:<12} {1}'.format('Spent today:', local_spent_today))
 
 
 if __name__ == '__main__':
