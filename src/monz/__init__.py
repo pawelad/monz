27247fe0e793ffd8725741614d69d12c7326ed0e
Paweł Adamczak <pawel.ad@gmail.com>
2024-02-16 12:36:49 +0100

    Update project metadata

--- src/monz/__init__.py
+++ src/monz/__init__.py
@@ -1,3 +1,9 @@
 """Simple CLI for your Monzo account."""
 
+__title__ = "monz"
+__description__ = "Simple CLI for your Monzo account."
 __version__ = "1.0.0.dev0"
+__url__ = "https://github.com/pawelad/monz"
+__author__ = "Paweł Adamczak"
+__email__ = "pawel.ad@gmail.com"
+__license__ = "MPL-2.0"
